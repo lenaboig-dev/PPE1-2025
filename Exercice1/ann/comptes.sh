65b0f766f00bcccaae6075249af69c1004a313a8
lenaboig-dev <lena.boig@sorbonne-nouvelle.fr>
2025-10-08 11:47:27 +0200

    Ajout Exercice1

---
+++ Exercice1/ann/comptes.sh
@@ -0,0 +1,6 @@
+echo "Nombre de lieux en 2016:"
+cat 2016/* | grep Location | wc -l
+echo "Nombre de lieux en 2017:"
+cat 2017/* | grep Location | wc -l
+echo "Nombre de lieux en 2018:"
+cat 2018/* | grep Location | wc -l
